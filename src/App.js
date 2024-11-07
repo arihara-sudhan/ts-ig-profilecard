a51ec4e52aa670368d008a74b4707309582fc2df
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2024-11-07 06:50:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-interface IgCardProps {
-  imgUrl?: string,
-  userName: string,
-  postsCount: number,
-  followingCount: number,
-  bio: string
-}
-
-const IgCard: React.FC<IgCardProps> = (props) => {
+const IgCard = (props) => {
   const {
     userName,
     postsCount,
@@ -17,34 +9,34 @@ const IgCard: React.FC<IgCardProps> = (props) => {
     bio,
     imgUrl = "./fback.png"
   } = props;
-  const [followersCount, setFollowersCount] = useState<number>(777);
-  const [followText, setFollowText] = useState<"Follow" | "Following">("Follow");
+  const [followersCount, setFollowersCount] = useState(777);
+  const [followText, setFollowText] = useState("Follow");
 
-  useEffect(()=>{
-    followText === "Follow" ? setFollowersCount(followersCount-1) : setFollowersCount(followersCount+1);
-  }, [followText])
+  useEffect(() => {
+    followText === "Follow" ? setFollowersCount(followersCount - 1) : setFollowersCount(followersCount + 1);
+  }, [followText]);
 
-  const followUser = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const followUser = () => {
     followText === "Follow" ? setFollowText("Following") : setFollowText("Follow");
-  }
+  };
 
-  return <div className="card">
-            <img src={imgUrl} alt="Image"/>
-            <div>
-              <div className="name-button">
-                <h3>{userName}</h3>
-                <button onClick={followUser}>{followText}</button>
-              </div>
-              <div className="posts-meta">
-                <h4>{postsCount} Posts</h4>
-                <h4>{followersCount} Followers</h4>
-                <h4>{followingCount} Following</h4>
-              </div>
-              <span>{bio}</span>
-            </div>
-         </div>
-}
-
-
+  return (
+    <div className="card">
+      <img src={imgUrl} alt="Image" />
+      <div>
+        <div className="name-button">
+          <h3>{userName}</h3>
+          <button onClick={followUser}>{followText}</button>
+        </div>
+        <div className="posts-meta">
+          <h4>{postsCount} Posts</h4>
+          <h4>{followersCount} Followers</h4>
+          <h4>{followingCount} Following</h4>
+        </div>
+        <span>{bio}</span>
+      </div>
+    </div>
+  );
+};
 
 export default IgCard;
