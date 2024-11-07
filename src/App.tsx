bfa9fff83b9f77261e9de734a60593119a6cd8b0
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2024-11-07 06:45:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
-import RightBar from "./components/RightBar";
-import "./style.css";
+import { useState, useEffect } from "react";
+import "./App.css";
 
-const App : React.FC = () => {
-  return <div className="parentFlex">
-          <img src="assets/profile-img.jpg" alt="ProfileImage"/>
-          <RightBar/>
-        </div>
+interface IgCardProps {
+  imgUrl?: string;
+  userName: string;
+  postsCount: number;
+  followingCount: number;
+  bio: string;
 }
 
-export default App;
+enum FollowStatus {
+  FOLLOW = "Follow",
+  FOLLOWING = "Following"
+}
+
+const IgCard: React.FC<IgCardProps> = (props) => {
+  const {
+    userName,
+    postsCount,
+    followingCount,
+    bio,
+    imgUrl = "./fback.png"
+  } = props;
+
+  const [followersCount, setFollowersCount] = useState<number>(777);
+  const [followText, setFollowText] = useState<FollowStatus>(FollowStatus.FOLLOW);
+
+  useEffect(() => {
+    setFollowersCount((prevCount) =>
+      followText === FollowStatus.FOLLOW ? prevCount - 1 : prevCount + 1
+    );
+  }, [followText]);
+
+  const followUser = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setFollowText(
+      followText === FollowStatus.FOLLOW ? FollowStatus.FOLLOWING : FollowStatus.FOLLOW
+    );
+  };
+
+  return (
+    <div className="card">
+      <img src={imgUrl} alt="User" />
+      <div>
+        <div className="name-button">
+          <h3>{userName}</h3>
+          <button onClick={followUser}>{followText}</button>
+        </div>
+        <div className="posts-meta">
+          <h4>{postsCount} Posts</h4>
+          <h4>{followersCount} Followers</h4>
+          <h4>{followingCount} Following</h4>
+        </div>
+        <span>{bio}</span>
+      </div>
+    </div>
+  );
+};
+
+export default IgCard;
