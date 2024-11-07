87a080dc3ecc892a2e05f04b3279dbeda4034f43
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2024-11-07 06:52:02 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -6,8 +5,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+  <center>
+    <App imgUrl="/ss1.png" userName="aravind_ariharasudhan" postsCount={21} followingCount={35} bio="I am not a bad guy; I am not a good guy; I am just THE GUY!"/>
+    <App imgUrl="/ss2.png" userName="ariharasudhan_ariharan" postsCount={21} followingCount={35} bio="I am not a bad guy; I am not a good guy; I am just THE GUY!"/>
+    <App imgUrl="/ss7.png" userName="arvin_ariharan_arvin" postsCount={21} followingCount={35} bio="I am not a bad guy; I am not a good guy; I am just THE GUY!"/>
+  </center>
 );
 
