9fa9a21f13be971872385d9b71936115723e1031
Southern Boy <100225369+arihara-sudhan@users.noreply.github.com>
2024-11-07 06:47:28 +0530

    Delete src/components directory

--- src/components/BioDetail.tsx
+++
@@ -1,9 +0,0 @@
-const BioDetail: React.FC = () => {
-    return <div>
-        <h4>ＡＲＩＨＡＲＡＳＵＤＨＡＮ</h4>
-        <h5>𝗦𝗸𝘆'𝘀 𝗧𝗵𝗲 𝗟𝗶𝗺𝗶𝘁 𝗪𝗵𝗲𝗻 𝗬𝗼𝘂'𝗿𝗲 𝗡𝗲𝘃𝗲𝗿 𝗚𝗶𝘃𝗶𝗻𝗴 𝗜𝗻 🔥</h5>
-        <a href="arihara-sudhan.github.io">arihara-sudhan.github.io</a>
-    </div>
-}
-
-export default BioDetail;
